d26ee47ffeb43d821d2ff6f3246768d4980fc18c
ChachaNy <nancy.zappellaz@students.hevs.ch>
2016-06-03 11:45:07 +0200

    1er commit

---
+++ Projet/src/main/java/Album.java
@@ -0,0 +1,96 @@
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="Album")
+public class Album {
+
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private long id;
+	@Column(name="titre")
+	private String title;
+	@Column(name="annee")
+	private int year;
+
+
+	// id
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+
+    // title
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	// year
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+
+	//constructors
+	public Album() {
+	}
+	public Album(String title, int year) {
+		this.title = title;
+		this.year = year;
+	}
+	
+	
+	
+	// relations
+	@ManyToMany(cascade = CascadeType.ALL)
+	private Set<Song> songs;
+	
+	@ManyToMany(cascade = CascadeType.ALL)
+	private Set<Artist> artists;
+	
+	@ManyToMany(cascade = CascadeType.ALL)
+	private Set<Type> types;
+		
+	// types (From Type)
+	public Set<Type> getTypes() {
+		return types;
+	}
+	public void setTypes(Set<Type> types) {
+		this.types = types;
+	}
+	
+	//artists (From Artist)	
+	public Set<Artist> getArtists() {
+			return artists;
+		}
+	public void setArtists(Set<Artist> artists) {
+		this.artists = artists;
+	}
+		
+	//songs (From Song) 
+	public Set<Song> getSongs() {
+		return songs;
+	}
+	public void setSongs(Set<Song> songs) {
+		this.songs = songs;
+	}
+
+}
---
+++ Projet/src/main/java/Artist.java
@@ -0,0 +1,81 @@
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Artiste")
+public class Artist {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private long id;
+	@Column(name="prenom")
+	private String name;
+	@Column(name="nom")
+	private String lastname;
+	@Column(name="groupe")
+	private boolean band;
+
+	// id
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	// name
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	// lastname
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	
+	// band
+	public boolean getBand() {
+		return band;
+	}
+	public void setBand(boolean band) {
+		this.band = band;
+	}
+	
+	//constructors
+	public Artist() {
+	}
+	public Artist(String name, String lastname, boolean band) {
+		this.name = name;
+		this.lastname = lastname;
+		this.band = band; 
+	}
+
+	
+	// relations
+	@ManyToMany(mappedBy = "artists", cascade = CascadeType.ALL)
+	private Set<Album> albums;
+
+	// albums (From Album) 
+	public Set<Album> getAlbums() {
+		return albums;
+	}
+	public void setAlbums(Set<Album> albums) {
+		this.albums = albums;
+	}
+	
+	
+}
